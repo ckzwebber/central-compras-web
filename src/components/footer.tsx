c6c934e835b39b724123516ba2e75e912eb4d461
ckzwebber <kacowebber11@outlook.com>
2025-10-26 11:04:26 -0300

    feat: add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
+import { Separator } from "./ui/separator";
+
 export const Footer = () => {
   return (
-    <footer className="w-full bg-zinc-950 pt-6 pb-6 pr-6 pl-6 text-white flex items-center justify-center hover:shadow-lg shadow-black/20 transition-all duration-300">
-      <p className="text-sm text-zinc-500">&copy; 2024 GURI'S STORE. All rights reserved.</p>
+    <footer className="w-full bg-zinc-950 pt-6 pb-6 pr-10 pl-10 text-white flex flex-col items-center justify-center hover:shadow-lg shadow-black/20 transition-all duration-300">
+      <Separator className="mb-4 bg-zinc-700" />
+      <p className="text-sm text-zinc-500">&copy; 2025 GURI'S STORE. All rights reserved.</p>
     </footer>
   );
 };
